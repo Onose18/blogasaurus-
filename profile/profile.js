59c1344eab54a8d5e1e891ce8eb83e0275bd4d1c
Demo User <demouser@MacBook-Air.local>
2017-07-19 17:41:23 -0400

    Adding this profile page onto my blog

---
+++ profile/profile.js
@@ -0,0 +1,15 @@
+function fullNameInput(){
+  var fullName= $('#fullName').val();
+}
+
+function userNameInput(){
+  var fullName= $('#userName').val();
+}
+
+function ageInput(){
+  var fullName= $('#age').val();
+}
+
+function passwordInput(){
+  var fullName= $('#password').val();
+}
